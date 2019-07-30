b733df58ab0a99e9a906e56d45043ab3ca50f7e0
roverchen <rover.k.chen@gmail.com>
2019-07-30 17:30:06 +0800

    <?php 			/* translators: used between list items, there is a space after the comma */ 		 	$categories_list = get_the_category_list( __( ', ', 'dgc' ) ); 			if ( $categories_list && dgc_categorized_blog() ) : ?> 			<span class="cat-links"> 				<?php printf( __( 'Posted in %1$s', 'dgc' ), $categories_list ); ?> 			</span> 			<?php endif; // End if categories ?>
    			<?php
    			/* translators: used between list items, there is a space after the comma */
    			$tags_list = get_the_tag_list( '', __( ', ', 'dgc' ) );
    			if ( $tags_list ) : ?>
    			<span class="tag-links">
    				<?php // printf( __( 'Tagged %1$s', 'dgc' ), $tags_list ); ?>
    				<?php echo $tags_list; ?>
    			</span>
    			<?php endif; // End if $tags_list ?>

--- functions.php
+++ functions.php
@@ -1660,20 +1660,6 @@ if ( ! function_exists( 'dgc_filter_refine' ) ) {
 			<?php endif; // End if $tags_list ?>
 		
 		<?php
-/*		if ( has_post_format( array( 'chat', 'status' ) ) )
-			$format_prefix = _x( '%1$s on %2$s', '1: post format name. 2: date', 'dgc' );
-		else
-			$format_prefix = '%2$s';
-
-		$date = sprintf( '<span class="date"><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a></span>',
-		esc_url( get_permalink() ),
-		esc_attr( sprintf( __( 'Permalink to %s', 'dgc' ), the_title_attribute( 'echo=0' ) ) ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( sprintf( $format_prefix, get_post_format_string( get_post_format() ), get_the_date() ) )
-		);
-
-		if ($echo ) echo $date;
-		return $date;*/
 	}
 }
 
