7bccbd409e290d9fd802436411950b0da48295e1
Rover Chen <rover@Rover-Ma.local>
2019-08-18 07:37:23 +0800

    $btn_cart = '<div class="cart-button">

--- functions.php
+++ functions.php
@@ -430,7 +430,8 @@ if ( ! function_exists( 'dgc_get_cart_button_html' ) ) {
 		if (class_exists('Woocommerce')) { 
 			global $woocommerce;
 			if (!empty($theme_options['showcart']) && (esc_attr($theme_options['showcart']) == 'on')) {
-					$btn_cart = '<div class="cart-button">
+					//$btn_cart = '<div class="cart-button">
+					$btn_cart = '<div class="menu-item">
 						<a href="'.get_permalink( wc_get_page_id( 'cart' ) ).'" class="cart-contents">
 							<div class="cart_image"></div> 
 							<span class="num_of_product_cart">'.$woocommerce->cart->cart_contents_count.'</span>
@@ -451,7 +452,7 @@ if ( ! function_exists( 'dgc_get_user_profile_html' ) ) {
 		if (class_exists('Woocommerce')) { 
 			global $woocommerce;
 			//if (!empty($theme_options['showuser']) && (esc_attr($theme_options['showuser']) == 'on')) {
-					$btn_user_profile = '<div class="user-profile">
+					$btn_user_profile = '<div class="menu-item">
 						<a href="'.get_permalink( wc_get_page_id( 'cart' ) ).'>
 							<div class="user_profile_image"></div> 
 						</a>
