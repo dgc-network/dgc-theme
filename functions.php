f82cf040d073bd2725ecfd1014a4082c2f00fd65
Rover Chen <rover@Rover-Ma.local>
2019-08-22 22:12:29 +0800

    #global_image{ 	background: url(images/global.png) no-repeat local 50% 50%; }

--- functions.php
+++ functions.php
@@ -1672,7 +1672,7 @@ if ( ! function_exists( 'dgc_get_qtranslate_languages_list' ) ) {
 				//echo '<div class="cart-button">';
 					echo '<ul id="lang-select-block">';
 						echo '<li class="current">';
-							echo '<div class="global_image">Language</div>';
+							echo '<div id="global_image">Language</div>';
 							echo '<ul id="lang-select-popup">';					
 							foreach($languages as $language){
 								echo '<li class="unactive">';
