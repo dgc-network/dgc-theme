df8ee350cf655195316fba7c57e149c1585c7467
roverchen <rover.k.chen@gmail.com>
2019-08-22 12:44:14 +0800

    padding: 10;

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 
 			<style type="text/css">
 			* {margin: 0; padding: 10;}
-			#footer-widgets {height: 100%; width:100%; font-size: 0; background: gray; padding: 10;}
+			#footer-widgets {height: 100%; width:100%; font-size: 0; background: gray;}
 			#footer-widget-1, #footer-widget-2, #footer-widget-3, #footer-widget-4 {display: inline-block; *display: inline; zoom: 1; vertical-align: top; font-size: 12px;}
 			#footer-widget-1 {width: 30%;}
 			#footer-widget-2 {width: 30%;}
