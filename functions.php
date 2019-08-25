72c13559914cc17d86566973c73f01b2e19d8531
Rover Chen <rover@Rover-Ma.local>
2019-08-25 18:19:43 +0800

    remove USER

--- functions.php
+++ functions.php
@@ -1654,7 +1654,7 @@ if ( ! function_exists( 'dgc_get_my_account_html' ) ) {
 	function dgc_get_my_account_html() {
 		$btn_my_account = '<div class="my-account">
 			<a href="'.get_permalink( wc_get_page_id( 'myaccount' ) ).'">
-				<div class="my-account-image">USER</div>
+				<div class="my-account-image"></div>
 			</a></div>';
 		echo $btn_my_account;
 	}
