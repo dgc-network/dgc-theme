4b89edfe47772459ac2d8833e0a13846949672cd
Rover Chen <rover@Rover-Ma.local>
2019-08-29 22:39:44 +0800

    display: flex;

--- functions.php
+++ functions.php
@@ -1520,10 +1520,12 @@ if (class_exists('Woocommerce')) {
 				get_product_search_form();
 			echo '</div>';
 		echo '</div>';
-		echo '<div class="dgc-register-warning">';
+		echo '<div class="dgc-register-section">';
+			echo '<div class="dgc-register-warning">';
 			echo '<div class="my-account-image"></div>';
 			echo '<p>As a member of EDD, you will be seamless with the world standard.</p>';
 			echo '<div class="my-account-image"></div>';
+			echo '</div>';
 		echo '</div>';
 	}
 	add_shortcode('dgc-product-search','dgc_product_search_page');
