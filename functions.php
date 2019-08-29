fdae5d6b87f5c0e7299034e47d1909db07add1f2
Rover Chen <rover@Rover-Ma.local>
2019-08-29 22:02:52 +0800

    background: url(https://lh3.googleusercontent.com/XLSaj41R-gU740qi2y5lQWL4SB5eJ1Js6XPOYBPDVu9I9LlzyHa1cZr5esVlPJzQHBvKD_5KBPyH8JJD_88h8AUvH2h-7oPeguHBsNBRnhJaOkPe2nF7crV40q_y81Xcf145FFXnJWHhumS2J0Ydmmdt-8HSegGHGQG3-pMZkolvKqn_bTjFpP4BWOSm2WL60j7jKvv3s-MwTsjKath81pnCaMIIqy5Pl9oVbHmv4HkR8Y7xSVFCLEBVsY1kCngAHHgoM4YFduoP68FmnG1sZbJYbfyjOdM8fXDEkKEGcHliESVxpUnUbQqxfirMVlan0PdFs1_5ASN1wfVqjf2x_j7s_olBz18Uycb2VwVIdRp0dCwTwimaXU1xV24Z1DgCNhsxBdFiTEex9-Kku8QyGl22UY0npIb4T8yUp5HvzXUfWHqUwFWPrpZ9rSUQCRsZLUaa6lYBxMk5WaHE69GObPgTEu0LaIB_gZWfsuDD0ZxR_j6pNLl8I8cWtDekCXbf1IGLJROgJw2YMTb0-3XHbZRV_nvwE3cH0rBNs1P3TtrpGMQAFnLFHo-VeKhX78MdU-naVd8DDye2RpQo51gGDmbJqb9A7oTCBgfIAC5iHvKfU6NQTiMbC9y492LHcDTFkZwED52MfrbxAc_gElgGJtomkJMeprlW=w1182-h886-no)

--- functions.php
+++ functions.php
@@ -1515,15 +1515,15 @@ if (class_exists('Woocommerce')) {
 
 	/*custom search page*/
 	function dgc_product_search_page() {
-		echo '<div class=dgc-search-page>';
-			echo '<div class=dgc-search-form>';
+		echo '<div class="dgc-search-page">';
+			echo '<div class="dgc-search-form">';
 				get_product_search_form();
 			echo '</div>';
 		echo '</div>';
-		echo '<div class=dgc-register-warning>';
-			echo '<div class=my-account-image></div>';
+		echo '<div class="dgc-register-warning">';
+			echo '<div class="my-account-image"></div>';
 			echo '<p>As a member of EDD, you will be seamless with the world standard.</p>';
-			echo '<div class=my-account-image></div>';
+			echo '<div class="my-account-image"></div>';
 	echo '</div>';
 }
 	add_shortcode('dgc-product-search','dgc_product_search_page');
