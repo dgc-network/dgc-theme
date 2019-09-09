9b455b9a6474bf5bddd233fb11f62843286b0cac
roverchen <rover.k.chen@gmail.com>
2019-09-09 09:20:15 +0800

    echo '<div class="dgc-search-section">';

--- functions.php
+++ functions.php
@@ -1517,10 +1517,11 @@ if (class_exists('Woocommerce')) {
 
 	/* custom search page included the search section and register section */
 	function dgc_product_search_page() {
-		echo '<div class="dgc-search-section">';
-			if (!empty($theme_options['show_feature_image']) && (esc_attr($theme_options['show_feature_image']) == 'on')) {
-				echo '<div class="search-section-image"></div>';
-			};
+		if (!empty($theme_options['show_feature_image']) && (esc_attr($theme_options['show_feature_image']) == 'on')) {
+			echo '<div class="dgc-search-section-with-image">';
+		} else {
+			echo '<div class="dgc-search-section">';
+		}
 
 			echo '<div class="dgc-search-bar">';
 				//echo '<div id="dgc-search-icon"><div class="fas fa-search"></div></div>';			
